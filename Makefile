7a2e7cfee90ad194c026981292b2f950329f933f
Mikzuit <mikexloo@hotmail.com>
2024-07-06 21:53:11 +0200

    :tada: Added first files

---
+++ Makefile
@@ -0,0 +1,43 @@
+.PHONY: kc_patch_pod min_start kc_debug_pod kc_debug_node
+
+kc_to_yaml:
+	kubectl get pod $(pod) -n $(ns) -o yaml > $(pod).yaml
+
+kc_patch_pod:
+	kubectl patch pod kube-apiserver-minikube -n kube-system --patch-file kube-apiserver.yaml
+
+kc_debug_pod:
+	@echo "Building debugging pod to pod: $(pod)"
+	kubectl debug $(pod) -it --image=busybox -- sh
+
+kc_debug_node:
+	@echo "Building debugging pod to node: $(node)"
+	kubectl debug $(node) -it --image=busybox -- sh
+
+min_start:
+	@echo "Building Minikube with $(K_VS)"
+#minikube start --memory 8182 --cpus 4 --kubernetes-version=v$(K_VS)
+	minikube start --memory 13082 --cpus 7 --kubernetes-version=v$(K_VS) \
+		--feature-gates=InPlacePodVerticalScaling=true \
+		--extra-config=controller-manager.horizontal-pod-autoscaler-upscale-delay=1m \
+  	--extra-config=controller-manager.horizontal-pod-autoscaler-downscale-delay=2m \
+  	--extra-config=controller-manager.horizontal-pod-autoscaler-sync-period=10s
+
+kc_pf_grafana:
+#kubectl port-forward -n monitoring svc/kube-prometheus-stack-grafana 3000:80 &> kubectl-port-forward.log &
+	nohup kubectl port-forward -n monitoring svc/kube-prometheus-stack-grafana 3000:80 &> kubectl-port-forward.log &
+
+kc_pods_metrics:
+	@echo "Getting pod metrics json with jq..."
+	kubectl get --raw "/apis/metrics.k8s.io/v1beta1/pods" | jq .
+
+kc_node_metrics:
+	@echo "Getting NODE metrics json with jq..."
+	kubectl get --raw "/apis/metrics.k8s.io/v1beta1/node" | jq .
+
+# kubectl port-forward -n monitoring svc/kube-prometheus-stack-grafana 3000:80
+# kubectl get --raw "/apis/metrics.k8s.io/v1beta1/nodes" | jq .
+
+kc_res_lim_ns:
+	@echo "Creating resource limited namespace"
+	kubectl apply -f manifests/ns-tools/resource-limited-ns.yaml
---
+++ iac/main.tf
@@ -0,0 +1,94 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "kube_prometheus_stack" {
+  name             = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "51.1.0"
+  create_namespace = true
+  namespace        = "tools-limited-namespace"
+  values = [
+    file("../helm/kube-prometheus-stack/Values.yaml")
+  ]
+
+  timeout = 180
+  depends_on = [ helm_release.vertical_pod_autoscaler ]
+}
+
+resource "helm_release" "metrics_server" {
+  name             = "metrics-server"
+  repository       = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart            = "metrics-server"
+  version          = "3.12.1"
+  create_namespace = true
+  namespace = "tools-limited-namespace"
+  values = [
+    file("../helm/metrics-server/Values.yaml")
+  ]
+
+  timeout = 180
+}
+
+resource "helm_release" "prometheus_adapter" {
+  name             = "prometheus-adapter"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "prometheus-adapter"
+  version          = "4.10.0"
+  create_namespace = true
+  namespace = "tools-limited-namespace"
+  values = [
+    file("../helm/prometheus-adapter/Values.yaml")
+  ]
+
+  timeout = 180
+  depends_on = [ helm_release.kube_prometheus_stack ]
+}
+
+resource "helm_release" "vertical_pod_autoscaler" {
+  name             = "vertical-pod-autoscaler"
+  repository       = "https://cowboysysop.github.io/charts/"
+  chart            = "vertical-pod-autoscaler"
+  version          = "9.8.2"
+  create_namespace = true
+  namespace = "tools-limited-namespace"
+  values = [
+    file("../helm/vertical-pod-autoscaler/Values.yaml")
+  ]
+
+  timeout = 300
+  depends_on = [ helm_release.metrics_server ]
+}
+
+/*
+data "kubernetes_service" "grafana" {
+  depends_on = [ helm_release.kube_prometheus_stack ]
+  metadata {
+    name = "kube-prometheus-stack-grafana"
+    namespace = "monitoring"
+  }
+}
+
+resource "null_resource" "kubectl_port_forward" {
+  count = 1
+  depends_on = [ 
+    helm_release.kube_prometheus_stack, 
+    data.kubernetes_service.grafana
+  ]
+  triggers = {
+    service_status = data.kubernetes_service.grafana.spec[0].cluster_ip
+  }
+
+  provisioner "local-exec" {
+    #command = "kubectl get svc -n monitoring"
+    #command = "kubectl port-forward -n monitoring svc/kube-prometheus-stack-grafana 3000:80"
+    command = "kubectl port-forward svc/kube-prometheus-stack-grafana 3000:80 &> /dev/null &"
+  }
+} */
